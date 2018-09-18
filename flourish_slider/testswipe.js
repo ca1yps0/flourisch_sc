97a44a5d3a95c2d1fdd3c37c797d01bdbe786494
Ioana <ioana@serenitymedia.ro>
2018-09-18 17:24:30 +0300

    mobile test 7

--- flourish_slider/testswipe.js
+++ flourish_slider/testswipe.js
@@ -1,10 +1,10 @@
 function addToConsole(message) {
-  $('.fconsole').append("<p>"+message+"</p><br>");
+  $('.box').append("<p>"+message+"</p><br>");
 }
 
 $(function(){
   // Bind the swipeHandler callback function to the swipe event on div.box
-  $( ".fconsole" ).on( "swipe", swipeHandler );
+  $( ".box" ).on( "swipe", swipeHandler );
 
   // Callback function references the event target and adds the 'swipe' class to it
   function swipeHandler( event ){
